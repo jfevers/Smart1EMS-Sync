66aa71467d498cf7513fd125f810aa27b8c44750
jafeverssenne@testo.de <jafeverssenne@testo.de>
2020-11-23 15:22:12 +0100

    new data with real values, refactoring of code to class

--- src/Experiment.py
+++ src/Experiment.py
@@ -4,135 +4,148 @@ import csv
 import re
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-#%%
-dictCounterNames = {}
-# buscounter
-dictCounterNames[1526658041] = 'Netzfrequzne [Hz]'
-dictCounterNames[1526471785] = 'WP Bezug' # 0
-dictCounterNames[1526466886] = 'Bezug' # 
-dictCounterNames[1526466858] = 'Überschuss' # ? korreliert mit 1526467213 und 1526471371
-# dictCounterNames[1526467140] = 'Heizstab Bezug' # 0
-
-# calculation counter
-#dictCounterNames[1526467264] = 'Bezug-HZ' # 0
-#dictCounterNames[1526467826] = 'Bezug-HZ-ECAR' # 0 
-#dictCounterNames[1526467678] = 'Überschuss+HZ+ECAR' # 0 
-dictCounterNames[1526477230] = 'Gesamtverbrauch' # 0
-dictCounterNames[1526467213] = 'Überschuss+HZ' # 500 - 2500
-dictCounterNames[1526477645] = 'Hausstrom' # 0 
-dictCounterNames[1526471371] = 'Überschuss+HZ+ECAR+Laden-Entlade' 
-#dictCounterNames[1526477597] = 'Wärmestrom' # 0 
-dictCounterNames[1526471474] = 'Bezug-HZ-ECAR-Discharge+Charge'
-
-
-# regulation
-dictCounterNames[1526495469] = 'BATT Power'
-dictCounterNames[1526495508] = 'Grid Power'
-
-# remote counter
-dictCounterNames[1526469030] = 'BATT Laden'
-dictCounterNames[1532951436] = 'PV DC Seitig'
-
-# remotesensor
-dictCounterNames[1526468849] = 'BATT V'
-dictCounterNames[1526468909] = 'Max Laden' # const 3000
-dictCounterNames[1526471294] = 'BATT A'
-dictCounterNames[1526468812] = 'BATT Ladezustand'
-dictCounterNames[1526468926] = 'Max Entladen'
-
-# update names from file
-#strFName='/home/friso/src/EMS-Readout/src/Name-mapping.txt'
-#f = open(strFName)
-#for line in f:
-#    ms=re.match('.*_(\d*)_Name=(.*)',line)
-#    if ms:
-#        id = int(ms[1])
-#        #print('ID: {} Name: {}'.format(id,ms[2]))
-#        foundID=dictCounterNames.get(id)
-#        if foundID:
-#            dictCounterNames[id] = ms[2]
-
-
-#for id in dictCounterNames.keys():
-#    print('ID: {}  Name: {}'.format(id,dictCounterNames[id]))
+import mysql.connector
 
 
 
 
+class EMSDataTransfer:
+  def __init__(self):
+    self.dictCounterNames = {}
+    self.strBasedir = '/home/friso/src/EMS-Readout/Testdata/FileDB/'
+
+    # buscounter
+    self.dictCounterNames[1526658041] = 'Netzfrequzne [Hz]'
+    self.dictCounterNames[1526471785] = 'WP Bezug' # 0
+    self.dictCounterNames[1526466886] = 'Bezug' # 
+    self.dictCounterNames[1526466858] = 'Überschuss' # ? korreliert mit 1526467213 und 1526471371
+    self.dictCounterNames[1526467140] = 'Heizstab Bezug' # 0
+
+    # calculation counter
+    self.dictCounterNames[1526467264] = 'Bezug-HZ' # 0
+    self.dictCounterNames[1526467826] = 'Bezug-HZ-ECAR' # 0 
+    self.dictCounterNames[1526467678] = 'Überschuss+HZ+ECAR' # 0 
+    self.dictCounterNames[1526477230] = 'Gesamtverbrauch' # 0
+    self.dictCounterNames[1526467213] = 'Überschuss+HZ' # 500 - 2500
+    self.dictCounterNames[1526477645] = 'Hausstrom' # 0 
+    self.dictCounterNames[1526471371] = 'Überschuss+HZ+ECAR+Laden-Entlade' 
+    self.dictCounterNames[1526477597] = 'Wärmestrom' # 0 
+    self.dictCounterNames[1526471474] = 'Bezug-HZ-ECAR-Discharge+Charge'
+    # regulation
+    self.dictCounterNames[1526495469] = 'BATT Power'
+    self.dictCounterNames[1526495508] = 'Grid Power'
+
+    # remote counter
+    self.dictCounterNames[1526469030] = 'BATT Laden'
+    self.dictCounterNames[1532951436] = 'PV DC Seitig'
+    # pv_global
+    self.dictCounterNames[1526476850] = 'PV Erzeugung'
+
+    # remotesensor
+    self.dictCounterNames[1526468849] = 'BATT V'
+    self.dictCounterNames[1526468909] = 'Max Laden' # const 3000
+    self.dictCounterNames[1526471294] = 'BATT A'
+    self.dictCounterNames[1526468812] = 'BATT Ladezustand'
+    self.dictCounterNames[1526468926] = 'Max Entladen'
+
+    self.dictCounterData ={} 
+
+
+  def updateIdMapping(self):
+#    strFName='/home/friso/src/EMS-Readout/src/Name-mapping.txt'
+#    f = open(strFName)
+#    for line in f:
+#        ms=re.match('.*_(\d*)_Name=(.*)',line)
+#        if ms:
+#            id = int(ms[1])
+#            #print('ID: {} Name: {}'.format(id,ms[2]))
+#            foundID=dictCounterNames.get(id)
+#            if foundID:
+#                dictCounterNames[id] = ms[2]
+
+    mydb = mysql.connector.connect(host="lala", user="EMSrw", password="NMe47u27gzsa", database="EMSdata")
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW TABLES")
+    for x in mycursor:
+      print(x) 
+
+    mycursor.execute("DROP TABLE IF EXISTS CounterNames")
+    mycursor.execute("CREATE TABLE CounterNames (counterId INT PRIMARY KEY, counterName VARCHAR(255), counterGroup VARCHAR(255))")
+
+    for id in self.dictCounterNames.keys():
+        name = self.dictCounterNames[id]
+        print('ID: {}  Name: {}'.format(id,name))
+        cmd = "INSERT INTO CounterNames (counterId,counterName,counterGroup) VALUES (\'{}\',\'{}\',\'{}\')".format(id,name,'group')
+        mycursor.execute(cmd)
+
+    mydb.commit()
+    mydb.close()
+
+
+  def updateOneCounterAllFiles(self,CounterId,dtNotBefore):
+    CounterName = self.dictCounterNames[CounterId]
+    #for globbing
+    strYear = '2020'
+    strMonth = '11'
+    strDay = '23'
+
+    allFilesForCounter = glob.glob(self.strBasedir+strYear+'/Linear/*'+str(CounterId)+'_global_'+strMonth+'_'+strDay+'_'+strYear+'.txt')
+
+    # for all date-files
+    for strCFile in allFilesForCounter:
+      ms = re.match('.*(\d*)_global_(\d*)_(\d*)_(\d*).txt',strCFile)
+      strDate=ms[4]+'-'+ms[2]+'-'+ms[3]
+      print('BC: {} Name: {}  date: {}'.format(CounterId,CounterName,strDate))
+      self.updateOneCounterOneFile(CounterId,strCFile,dtNotBefore)
 
 
 
 
 
 
-strBasedir = '/home/friso/src/EMS-Readout/Testdata/FileDB/'
+  def updateOneCounterOneFile(self,CounterId,strCFile,dtNotBefore):
+    lstTupDay = []
+    
+    with open(strCFile) as csvfile:
+        reader = csv.reader(csvfile,delimiter=';')
+        for row in reader:
+            strTS = row[0] # read timestamp
+            intTS = int(strTS) - 3600 # correct by -1 hour
+            strValue = row[1]
+            DT = datetime.datetime.fromtimestamp(intTS)  
+            fVal = float(strValue)
+            lstTupDay.append((intTS,DT,fVal))
+            # print('     strDT: {}  dt: {}   value: {}'.format(strTS,DT,fVal))
+    lstTupDay.sort(key=lambda tup: tup[0])
+    self.dictCounterData[CounterId].extend(lstTupDay)
+
+
+
+  def updateAllCounter(self):
+    ### for debugging only
+    # self.dictCounterNames.clear()
+    # self.dictCounterNames[1526466858] = 'Überschuss CGout[W]'
+
+    dtNotBefore = datetime.datetime(2020,11,23)
+
+    for CounterId in self.dictCounterNames.keys():
+      self.dictCounterData[CounterId] =[] 
+      self.updateOneCounterAllFiles(CounterId, dtNotBefore)
 
 
 
 
 
-
-
-
-### for debugging only
-#dictCounterNames.clear()
-#dictCounterNames[1526466858] = 'Überschuss CGout[W]'
-
+myUpdater = EMSDataTransfer()
+# myUpdater.updateIdMapping()
+myUpdater.updateAllCounter()
 
 
 lstLegend = []
-
-#for strBCFile in fnBusCounter:
-#    ms = re.match('.*counter_(\d*)_global_(\d*)_(\d*)_(\d*).txt',strBCFile)
-#    BcId = int(ms[1])
-
-
-
-
-
-#for globbing
-strYear = '2020'
-strMonth = '11'
-strDay = '21'
-
-# for all registered counters
-for CounterId in dictCounterNames.keys():
-    lstDataCounter = []
-    CounterName = dictCounterNames[CounterId]
-    lstLegend.append(str(CounterId)+'  '+CounterName)
-    allFilesForCounter = glob.glob(strBasedir+strYear+'/Linear/*'+str(CounterId)+'_global_'+strMonth+'_'+strDay+'_'+strYear+'.txt')
-
-    # for all date-files
-    for strBCFile in allFilesForCounter:
-        ms = re.match('.*(\d*)_global_(\d*)_(\d*)_(\d*).txt',strBCFile)
-
-        strDate=ms[4]+'-'+ms[2]+'-'+ms[3]
-        print('BC: {} Name: {}  date: {}'.format(CounterId,CounterName,strDate))
-        lstTupDay = []
-        
-        with open(strBCFile) as csvfile:
-            reader = csv.reader(csvfile,delimiter=';')
-            for row in reader:
-                strTS = row[0] # read timestamp
-                intTS = int(strTS) - 3600 # correct by -1 hour
-                strValue = row[1]
-                DT = datetime.datetime.fromtimestamp(intTS)  
-                fVal = float(strValue)
-                lstTupDay.append((intTS,DT,fVal))
-                # print('     strDT: {}  dt: {}   value: {}'.format(strTS,DT,fVal))
-        lstTupDay.sort(key=lambda tup: tup[0])
-        lstDataCounter.extend(lstTupDay)
-    #    last=0
-    #    for tup in lstTup:
-    #        t = tup[0]
-    #        diff = t-last
-    #        print('{}   {}'.format(t,diff))
-    #        last=t
-     #   print(lstTup[-1])
-    data = np.array(lstDataCounter)
-    plt.plot(data[:,1],data[:,2],'.')
-
-plt.legend(lstLegend)
+for cid in myUpdater.dictCounterData.keys():
+  lstLegend.append(str(cid)+'  '+myUpdater.dictCounterNames[cid])
+  data = np.array(myUpdater.dictCounterData[cid])
+  plt.plot(data[:,1],data[:,2],'.')
+  plt.legend(lstLegend)
 plt.show()
